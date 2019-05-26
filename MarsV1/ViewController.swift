d140c1c7aaa5530589db95cc95df8d4837ad078f
2jungwon <james66won@gmail.com>
2019-05-26 22:57:23 +0900

    Initial Commit

---
+++ MarsV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsV1
+//
+//  Created by 이정원 on 26/05/2019.
+//  Copyright © 2019 Jungwon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
